cadda88da4a915ca63152a4253318b47b8750da9
Khizer Jamil <157282606+khizkj@users.noreply.github.com>
2025-05-04 15:01:48 -0700

    Create Lecture01Variables.dart

---
+++ Lecture01Variables.dart
@@ -0,0 +1,15 @@
+void main() {
+  
+  String shahadah = "There is no god but Allah, and Muhammad is His Messenger.";
+  String salah = "Praying five times a day.";
+  String zakat = "Giving charity to the needy.";
+  String sawm = "Fasting during Ramadan.";
+  String hajj = "Pilgrimage to Mecca once in a lifetime.";
+
+  print("Five Pillars of Islam:");
+  print("Shahadah: $shahadah");
+  print("Salah: $salah");
+  print("Zakat: $zakat");
+  print("Sawm: $sawm");
+  print("Hajj: $hajj");
+}
